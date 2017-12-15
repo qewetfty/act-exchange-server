98773f9379d3b8415bab96f703d96e04f5e92985
yujianjian <yujianjian@new4g.cn>
2017-12-15 16:09:06 +0800

    提交工程

---
+++ src/main/java/com/achain/conf/Config.java
@@ -0,0 +1,17 @@
+package com.achain.conf;
+
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author yujianjian
+ * @since 2017-11-29 下午5:22
+ */
+@Component
+@Slf4j
+public class Config {
+
+
+}
---
+++ src/main/java/com/achain/conf/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.achain.conf;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author yujian
+ */
+@Configuration
+@MapperScan("com.achain.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+
+}
