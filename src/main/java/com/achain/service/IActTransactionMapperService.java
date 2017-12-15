fd02d1738a5ca624c7f5bca03bce03ae7ba7fb43
fanyukun <fanyukun@new4g.cn>
2017-12-15 18:15:21 +0800

    transaction

--- src/main/java/com/achain/service/IActTransactionMapperService.java
+++ src/main/java/com/achain/service/IActTransactionMapperService.java
@@ -3,9 +3,12 @@ package com.achain.service;
 import com.achain.domain.entity.ActTransaction;
 import com.baomidou.mybatisplus.service.IService;
 
+import java.util.Map;
+
 /**
  * @author yujianjian
  * @since 2017-12-15 下午4:44
  */
 public interface IActTransactionMapperService extends IService<ActTransaction> {
+    Map<String, Object> WalletAccountTransactionHistory(Long start, String address);
 }
