dd220313a6cca696219ac538f75b98b60136023a
yujianjian <yujianjian@new4g.cn>
2017-12-15 18:55:04 +0800

    update

--- src/main/java/com/achain/service/impl/BlockchainServiceImpl.java
+++ src/main/java/com/achain/service/impl/BlockchainServiceImpl.java
@@ -378,15 +378,16 @@ public class BlockchainServiceImpl implements IBlockchainService {
                 actTransaction.setFromAcct(temp.getString("from_account_name"));
                 actTransaction.setToAcct(temp.getString("to_account_name"));
                 actTransaction.setToAddr(temp.getString("to_account"));
-                if(!config.checkActAddress.contains(actTransaction.getFromAddr()) &&
-                   !config.checkActAddress.contains(actTransaction.getToAddr())){
-                    return null;
-                }
                 actTransaction.setAmount(temp.getJSONObject("amount").getLong("amount"));
                 actTransaction.setFee(createTaskJson.getJSONObject("fee").getInteger("amount"));
                 actTransaction.setTrxTime(dealTime(createTaskJson.getString("timestamp")));
                 actTransaction.setMemo(temp.getString("memo"));
                 actTransaction.setIsCompleted((byte) 0);
+            }
+
+            if(!config.checkActAddress.contains(actTransaction.getFromAddr()) &&
+               !config.checkActAddress.contains(actTransaction.getToAddr())){
+                return null;
             }
             actTransaction.setBlockNum(createTaskJson.getLong("block_num"));
             actTransaction.setBlockPosition(createTaskJson.getInteger("block_position"));
